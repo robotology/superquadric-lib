d7c94f0e74fa6f47b08ce04a5861a961c2fd8f7d
Giulia Vezzani <vzzgli@gmail.com>
2019-02-01 14:17:59 +0100

    so far let's consider implementation correct

--- src/SuperquadricPipeline/multiple-superq/main.cpp
+++ src/SuperquadricPipeline/multiple-superq/main.cpp
@@ -71,7 +71,7 @@ int main(int argc, char* argv[])
 
     // Params for solver in grasp estimator
     IpoptParam iparams_grasp;
-    iparams_grasp.tol = 1e-4;
+    iparams_grasp.tol = 1e-5;
     iparams_grasp.constr_tol = 1e-4;
     iparams_grasp.acceptable_iter = 0;
     iparams_grasp.mu_strategy = "adaptive";
@@ -132,7 +132,7 @@ int main(int argc, char* argv[])
     // Params for grasp computation
     GraspParams params_grasp;
     params_grasp.left_or_right = "right";
-    params_grasp.pl << 0.0, 0.0, 1.0, 0.16;
+    params_grasp.pl << 0.0, 0.0, 1.0, 0.18;
     params_grasp.disp <<  0.0, 0.0, 0.0;
     params_grasp.object_superq = superqs[0];
     params_grasp.max_superq = 4;
@@ -140,10 +140,10 @@ int main(int argc, char* argv[])
     params_grasp.bounds_left << -0.5, 0.0, -0.2, 0.2, -0.3, 0.3,  -M_PI, M_PI,-M_PI, M_PI,-M_PI, M_PI;
     params_grasp.bounds_constr_left.resize(8,2);
     params_grasp.bounds_constr_left << -10000, -0.001, -10000, -0.001, -10000, -0.001, 0.01,
-                                        10.0, 0.0, 1.0, 0.000001, 10.0, 0.000001, 10.0, 0.000001, 10.0;
+                                        10.0, 0.0, 1.0, 0.00001, 10.0, 0.00001, 10.0, 0.00001, 10.0;
     params_grasp.bounds_constr_right.resize(8,2);
-    params_grasp.bounds_constr_right << -10000, 0.0, -10000, 0.0, -10000, 0.0, 0.01,
-                                        10.0, 0.0, 1.0, 0.000001, 10.0, 0.000001, 10.0, 0.000001, 10.0;
+    params_grasp.bounds_constr_right << -10000, 0.0, -10000, 0.0, -10000, 0.0, 0.001,
+                                        10.0, 0.0, 1.0, 0.00001, 10.0, 0.00001, 10.0, 0.00001, 10.0;
 
     Superquadric hand;
     Vector11d hand_vector;
